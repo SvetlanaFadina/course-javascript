b9deeeb3749f2eb9a5833def98eddb421fc9424a
Svetlana Fadina <s-fadina@yandex.ru>
2022-05-10 21:39:22 +0300

    changed after overview

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -23,7 +23,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || array.length <= 0) {
+  if (!Array.isArray(array) || array.length == 0) {
     throw new Error('empty array');
   } else if (typeof fn != 'function') {
     throw new Error('fn is not a function');
@@ -53,7 +53,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || array.length <= 0) {
+  if (!Array.isArray(array) || array.length == 0) {
     throw new Error('empty array');
   } else if (typeof fn != 'function') {
     throw new Error('fn is not a function');
@@ -78,7 +78,7 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn, ...args) {
-  if (typeof fn != 'function') {
+  if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
   }
 
@@ -112,51 +112,48 @@ function returnBadArguments(fn, ...args) {
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
 function calculator(number = 0) {
-  if (typeof number != 'number') {
+  if (!Number.isFinite(number)) {
     throw new Error('number is not a number');
   }
 
-  var ob = {
-    sum: function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] == 0) {
-          throw new Error('division by 0');
-        }
-        number += arguments[i];
+  return {
+    sum(...args) {
+      let result = number;
+
+      for (const arg of args) {
+        result += arg;
       }
-      return number;
+      return result;
     },
-    dif: function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] == 0) {
-          throw new Error('division by 0');
-        }
-        number -= arguments[i];
+    dif(...args) {
+      let result = number;
+
+      for (const arg of args) {
+        result -= arg;
       }
-      return number;
+      return result;
     },
-    div: function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] == 0) {
+    div(...args) {
+      let result = number;
+
+      for (const arg of args) {
+        if (arg === 0) {
           throw new Error('division by 0');
         }
-        number /= arguments[i];
+        result /= arg;
       }
-      return number;
+      return result;
     },
-    mul: function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] == 0) {
-          throw new Error('division by 0');
-        }
-        number *= arguments[i];
+    mul(...args) {
+      let result = number;
+
+      for (const arg of args) {
+        result *= arg;
       }
-      return number;
+      return result;
     },
   };
-  return ob;
 }
-
 /* При решении задач, постарайтесь использовать отладчик */
 
 export { isAllTrue, isSomeTrue, returnBadArguments, calculator };
